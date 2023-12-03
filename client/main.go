9d65185d15f9ec68e6e2d058d51926dff456a790
Harry Muir Camacho <hmuir@Harrys-MacBook-Pro.local>
2023-12-02 22:49:53 -0600

    Finished to implement client

--- client/main.go
+++ client/main.go
@@ -1,3 +1,33 @@
 package main
 
-func main() {}
+import (
+	"log"
+
+	pb "github.com/hmuir28/go-grpc-chat/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const (
+	port = ":8000"
+)
+
+func main() {
+
+	conn, err := grpc.Dial("localhost"+port, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+
+	defer conn.Close()
+
+	client := pb.NewGreetServiceClient(conn)
+
+	// names := &pb.NameList{
+	// 	Names: []string{ "Harry", "Michael", "Brian" }
+	// }
+
+	callSayHello(client)
+
+}
