2333e8fd54eb25d61c4b03c0e7120d9e7f2c30df
Harry Muir Camacho <hmuir@Harrys-MacBook-Pro.local>
2023-12-02 21:34:32 -0600

    Generated grpc proto files

--- server/main.go
+++ server/main.go
@@ -1,3 +1,30 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"net"
+	pb "github.com/hmuir28/go-grpc-chat/proto"
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":8000"
+)
+
+func main() {
+
+	lis, err := net.Listen("tcp", port)
+
+	if err != nil {
+		log.Fatalf("Failed to start the server %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+
+	pb
+
+	err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Failed to start: %v", err)
+	}
+
+}
